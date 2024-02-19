dbd7c938ce8092d493c5391ef66c377e1f8d8306
Stefani-web <stephaniecvlho@gmail.com>
2024-02-19 16:29:29 +0100

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints exactly
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
+
