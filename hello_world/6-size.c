1cb2b4a903ccb9338301397b60e3137718a9a676
Stefani-web <stephaniecvlho@gmail.com>
2024-02-20 15:01:34 +0100

    print size

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <studio.h>
+
+/**
+ * main - precise size a various type 
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	Size of a char: 1 byte(s)
+	Size of an int: 4 byte(s)
+	Size of a long int: 4 byte(s)
+	Size of a long long int: 8 byte(s)
+	Size of a float: 4 byte(s)
+}
+
