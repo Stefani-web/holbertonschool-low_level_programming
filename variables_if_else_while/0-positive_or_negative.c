bc8533d8bc8db72518ebce8aa78ff60df2b17e41
Stefani-web <stephaniecvlho@gmail.com>
2024-02-21 14:42:54 +0100

    print positivenegative

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - program that prints exactly
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+	int nombre;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (nombre > 0)
+	printf("le nombre est positif.\n");
+	if else printf("le nombre est négatif.\n");
+
+	return (0);
+}
