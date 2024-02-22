db9da38746ca83b553ff4244ae125c42f142556f
Stefani-web <stephaniecvlho@gmail.com>
2024-02-22 23:07:11 +0100

    print last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stio.h>
 
 /**
- * Generating a random number for the variable
+ * Main - function
+ * Generation a random number for the variable
  *
  * Return: 0
  */
@@ -11,17 +13,23 @@ int main(void)
 {
 	int n;
 	int stephy;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	stephy = n % 10;
-	
+
 	if (stephy > 5)
-		printf("%d and is greater than 5", stephy);
+{
+	printf("Last digit %d is %d and is greater than 5\n", n, stephy);
+}
 	else if (stephy == 0)
-		printf("%d and is 0", stephy)
+{
+	printf("Last digit %d and is 0\n", n, stephy);
+}
 	else
-		printf("%d and is less than 6 and not 0", stephy)
+{
+	printf("Last digit %d and is less than 6 and not 0\n", n, stephy)
+}
 
 	return (0);
 }
