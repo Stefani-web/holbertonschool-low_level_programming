be091f2ad52dd28c3abc4c8ba80c7a09377a748b
Stefani-web <stephaniecvlho@gmail.com>
2024-02-23 10:30:23 +0100

    print alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -13,10 +13,8 @@ int main(void)
 {
 char lettre;
 
-	for (lettre = 'a'; lettre >= 'Z'; lettre++)
-{
+	for (lettre = 'a'; lettre <= 'z'; lettre++)
 	putchar(lettre);
-}
 	putchar('\n');
 	return (0);
 }
