b781e2a3a47db3b34f82525e374f0f6e657ac2a3
Stefani-web <stephaniecvlho@gmail.com>
2024-02-23 20:40:02 +0100

    print number,

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		putchar(number);
+
+		if (number != '9')
+		{
+
+			putchar(',');
+			putchar(' ');
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
