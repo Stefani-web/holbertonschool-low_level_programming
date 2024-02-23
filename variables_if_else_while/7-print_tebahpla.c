9b7d0c51c3b37aa379bf76be683ec108827382af
Stefani-web <stephaniecvlho@gmail.com>
2024-02-23 19:05:48 +0100

    print alphabet reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+char lettre;
+
+	for (lettre = 'a'; lettre <= 'z'; lettre--)
+	{
+		putchar(lettre);
+	}
+	putchar('\n');
+	return (0);
+}
+
