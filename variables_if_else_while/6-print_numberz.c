33c6651fc59159c10875341b4efcaa57c9815a0b
Stefani-web <stephaniecvlho@gmail.com>
2024-02-23 18:58:40 +0100

    print number base 10

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		putchar('0' + number);
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
+
