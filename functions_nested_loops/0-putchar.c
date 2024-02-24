08ce027593c5e26448cbf36d44f23025015323df
Stefani-web <stephaniecvlho@gmail.com>
2024-02-24 14:25:38 +0100

    print _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,13 +1,17 @@
+#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - function print
+ * main - print _putchar
+ *
+ * This function prints "_putchar"
  *
  * Always return 0
+ *
  */
 
 int main(void)
+
 {
 	printf("_putchar\n");
 	return (0);
