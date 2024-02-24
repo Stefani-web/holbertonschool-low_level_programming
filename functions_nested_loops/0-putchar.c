617061eb4cdd611099cfb09bb9de79985b7a08c2
Stefani-web <stephaniecvlho@gmail.com>
2024-02-24 16:08:28 +0100

    print 0putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
 
 /**
- * main - function to print
+ * main - fonction principale
  *
- * Always return 0
- *
+ * Return: Always 0
  */
 
 int main(void)
