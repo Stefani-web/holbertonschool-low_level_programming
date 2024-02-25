82d3573008eaacfa7aa801945a08db0a13f8c147
Stefani-web <stephaniecvlho@gmail.com>
2024-02-25 15:01:22 +0100

    add prototype

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	putchar('\n');
+}
+
