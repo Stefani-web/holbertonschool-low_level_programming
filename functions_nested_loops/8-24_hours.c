837b7cfa0421e3234353fa0b78a4d5a39900480a
Stefani-web <stephaniecvlho@gmail.com>
2024-02-25 20:06:06 +0100

    add prototype

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Function that prints every minute of the day of Jack Bauer
+ *
+ * Return: 23 hours and 59 minutes
+ */
+
+void jack_bauer(void)
+{
+	int hours, minutes;
+
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (minutes = 0; minutes < 60; minutes++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
+
