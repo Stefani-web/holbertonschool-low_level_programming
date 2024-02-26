8c2476b8a6641aabfa344e312b6537432c506581
Stefani-web <stephaniecvlho@gmail.com>
2024-02-26 22:23:05 +0100

    add prototype

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Function that ultiplies two integers.
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Return: result of a and b
+ */
+
+int mul(int a, int b)
+
+{
+	return (a * b);
+}
+
