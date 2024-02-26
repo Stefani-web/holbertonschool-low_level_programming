f87673e66a4d67f0a4b47e4f185da63044274d0f
Stefani-web <stephaniecvlho@gmail.com>
2024-02-26 16:07:59 +0100

    add prototype

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - Function that checks for a digit (0 through 9).
+ * @c: the character bring checker
+ *
+ * Return: 1 and 0
+ */
+
+int _isdigit (int c)
+{
+	if (c >= '0' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
+
