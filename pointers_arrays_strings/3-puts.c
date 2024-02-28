ee86e17ee3186b98686681b907fca125bccd9dfd
Stefani-web <stephaniecvlho@gmail.com>
2024-02-28 15:22:02 +0100

    function that prints a string

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Function that prints a string
+ * @str: pointer string
+ *
+ * Return: string
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+	}
+	putchar('\n');
+}
+
