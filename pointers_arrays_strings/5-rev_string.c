d217ad4a67b39b8933e2a038f4f459521193ffdf
Stefani-web <stephaniecvlho@gmail.com>
2024-02-29 23:50:57 +0100

    rev_string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - Function that prints a string, in reverse
+ * @s: pointer
+ *
+ */
+
+void rev_string(char *s)
+{
+	int lenght = 0, index = 0;
+	char tempo;
+
+        while (s[lenght] != '\0')
+                lenght++;
+
+        for (index = lenght - 1; index >= lenght / 2; index--)
+        {
+		tempo = s[index];
+		s[index] = s[lenght - index - 1];
+		s[lenght - index - 1] = tempo;
+        }
+}
+
