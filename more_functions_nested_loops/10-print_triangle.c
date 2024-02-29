aa823112f385df7eb69bfa645808da3560097349
Stefani-web <stephaniecvlho@gmail.com>
2024-02-29 23:32:30 +0100

    print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle
+ * @size:
+ *
+ * Return: Always
+ */
+
+void print_triangle(int size)
+{
+	int ligne, colonne;
+
+	if (size > 0)
+	{
+		for (ligne = 1; ligne <= size; ligne++)
+		{
+			for (colonne = size - ligne; colonne > 0; colonne--)
+			{
+				_putchar('0');
+			}
+			for (colonne = ligne; colonne > 0; colonne--)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+
