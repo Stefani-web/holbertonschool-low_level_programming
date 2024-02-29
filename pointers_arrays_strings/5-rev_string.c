22adb6849e29c83843ebb7a18b3dab6241ba9e4a
Stefani-web <stephaniecvlho@gmail.com>
2024-02-29 23:56:36 +0100

    rev_string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -11,14 +11,16 @@ void rev_string(char *s)
 	int lenght = 0, index = 0;
 	char tempo;
 
-        while (s[lenght] != '\0')
-                lenght++;
+	while (s[lenght] != '\0')
+	{
+		lenght++;
+	}
 
-        for (index = lenght - 1; index >= lenght / 2; index--)
-        {
+	for (index = lenght - 1; index >= lenght / 2; index--)
+	{
 		tempo = s[index];
 		s[index] = s[lenght - index - 1];
 		s[lenght - index - 1] = tempo;
-        }
+	}
 }
 
