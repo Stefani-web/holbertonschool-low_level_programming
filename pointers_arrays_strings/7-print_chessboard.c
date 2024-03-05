adaa5d581681513d87c0a618922eba9d0ddad0ad
Stefani-web <stephaniecvlho@gmail.com>
2024-03-05 15:51:27 +0100

    chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int row, column;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (column = 0; column < 8; column++)
+		{
+			_putchar(a[row][column]);
+		}
+		_putchar('\n');
+	}
+
+}
+
