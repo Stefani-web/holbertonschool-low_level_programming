3b4038dfe4fcfdce317ebb258dbe613f61e6dd36
Stefani-web <stephaniecvlho@gmail.com>
2024-03-05 23:41:27 +0100

    print its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints its name
+ * @argv: Var that contains the num of arguments
+ * @argc: Var that contains the num of arguments
+ *
+ * Retur: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
+
