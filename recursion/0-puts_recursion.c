9b73c9e9f5dc7ceba8527b783a17ff22935902f8
Stefani-web <stephaniecvlho@gmail.com>
2024-03-05 22:53:46 +0100

    print a string

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - Function that prints a string
+ * @s: string print
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+			_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
+
