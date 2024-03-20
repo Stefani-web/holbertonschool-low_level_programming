8f9a391f70d42bd6977c712a0cf030f57c94b37e
Stefani-web <stephaniecvlho@gmail.com>
2024-03-20 15:58:09 +0100

    corrig task3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "calc.h"
-#include "op_functions.c"
+#include "3-calc.h"
 
 /**
  * main - entry point of the program
- * @argc: number of command line arguments
+ * @argc: num of command line arguments
  * @argv: array of command line arguments
  *
  * Return: 0 on success, error code otherwise
@@ -13,35 +12,31 @@
 
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
-	int (*op_func)(int, int);
+	int (*f)(int a, int b);
+	int a, b;
 
-	if (argc != 4)
+	if (argc < 4)
 	{
 		printf("Error\n");
 		return (98);
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-
-	op_func = get_op_func(argv[2][0]);
-
-	if (op_func == NULL)
+	f = get_op_func(argv[2]);
+	if (f == NULL)
 	{
 		printf("Error\n");
 		return (99);
 	}
 
-	result = op_func(num1, num2);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
-	if (result == -1)
+	if (*argv[2] == '/' && b ==0)
 	{
 		printf("Error\n");
 		return (100);
 	}
 
-	printf("%d\n", result);
-
+	printf("%d\n", f(a, b));
 	return (0);
 }
