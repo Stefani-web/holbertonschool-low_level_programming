c82c6c2191e252ffe924ffdfa4da07408597408e
Stefani-web <stephaniecvlho@gmail.com>
2024-03-20 16:18:05 +0100

    add proto

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -7,7 +7,6 @@
  * @op: The operator
  * @f: The function associated
  */
-
 typedef struct op
 {
 	char *op;
@@ -19,5 +18,6 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 
 #endif
