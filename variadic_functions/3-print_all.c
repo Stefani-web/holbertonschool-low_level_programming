82a8a4445b5e815909b1f2ba5c24cd71bed13cd8
Stefani-web <stephaniecvlho@gmail.com>
2024-03-22 22:43:57 +0100

    corrige the last if

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,9 +1,13 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include "variadic_functions.h"
 
 /**
- * print_all - prints anything
+ * print_all - Prints anything
  * @format: list of types of arguments passed to the function
+ * Details : This function will iterate over each character of the format
+ * string and print the next argument of the specified type
+ *
  */
 
 void print_all(const char * const format, ...)
@@ -11,38 +15,37 @@ void print_all(const char * const format, ...)
 	va_list args;
 	int i = 0;
 	char *str;
-	const char *separator = "";
 
 	va_start(args, format);
 
-	while (format && format[i])
+	while (format != NULL && format[i] != '\0')
 	{
 		switch (format[i])
 		{
 			case 'c':
-				printf("%s%c", separator, va_arg(args, int));
+				printf("%c", va_arg(args, int));
 				break;
 			case 'i':
-				printf("%s%d", separator, va_arg(args, int));
+				printf("%i", va_arg(args, int));
 				break;
 			case 'f':
-				printf("%s%f", separator, va_arg(args, double));
+				printf("%f", va_arg(args, double));
 				break;
 			case 's':
 				str = va_arg(args, char *);
-				if (!str)
-					printf("%s(nil)", separator);
-				else
-					printf("%s%s", separator, str);
-				break;
-			default:
-				i++;
-				continue;
+				if (str == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+					printf("%s", str);
+					break;
 		}
-		separator = ", ";
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
+		format[i] == 's') && format[i + 1] != '\0')
+			printf(", ");
 		i++;
 	}
-
 	printf("\n");
 	va_end(args);
 }
