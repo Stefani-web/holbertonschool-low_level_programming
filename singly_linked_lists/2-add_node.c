e81e20912e8fc53cb37305453328b8c33f81ef86
Stefani-web <stephaniecvlho@gmail.com>
2024-04-03 23:10:56 +0200

    change 1 notion strlen

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,12 +1,12 @@
-#include "lists.h"
 #include <string.h>
+#include "lists.h"
 
 /**
- * add_node - function that adds a new node at the beginning of a list_t list
- * @head: Double pointer to the head of the list_t list
- * @str: String to be duplicated and stored in the new node
+ * add_node - Function that adds a new node at the beginning of a list_t list
+ * @head: double pointer to the head of the list
+ * @str: string to be duplicated and stored in the new node
  *
- * Return: The address of the new element, or NULL if it failed
+ * Return: the address of the new element, or NULL if it failed
  */
 
 list_t *add_node(list_t **head, const char *str)
@@ -15,9 +15,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-	{
-		return (NULL); /* Return NULL if malloc fails */
-	}
+		return (NULL);
 
 	/* Duplicate the string using strdup and store it in the new node */
 	new_node->str = strdup(str);
@@ -27,10 +25,8 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	/* Set the new node's next pointer to the current head of the list */
-
+	new_node->len = strlen(str);
 	new_node->next = *head;
-
 	*head = new_node;
 
 	return (new_node);
