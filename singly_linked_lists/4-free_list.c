9bcb04c5c1e35cd9d6a62c304ee73368e1482fd5
Stefani-web <stephaniecvlho@gmail.com>
2024-04-03 19:47:24 +0200

    function that frees a list_t list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -10,10 +10,10 @@ void free_list(list_t *head)
 	list_t *current; /* Declare a pointer to the current node */
 
 	/* Loop through the list and free each node */
-	while (head != NULL)
+	while (head)
 	{
 		current = head; /* Set the current node to the head of the list */
-		head = head->next; /* Move the head to the next node */
+		head = current->next; /* Move the head to the next node */
 		free(current->str); /* Free the string stored in the current node */
 		free(current); /* Free the current node */
 	}
