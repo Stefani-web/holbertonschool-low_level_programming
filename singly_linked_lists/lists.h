eb966934b5be6a162c7c492a779cccfaf012aec9
Stefani-web <stephaniecvlho@gmail.com>
2024-04-04 12:09:19 +0200

    add betty doc

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -6,7 +6,12 @@
 #include <string.h>
 
 /**
- * structure representing a list element
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
  */
 typedef struct list_s
 {
