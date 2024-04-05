24670cfc31d2e90b3ea16447760cc8670c1b9dcf
Stefani-web <stephaniecvlho@gmail.com>
2024-04-05 16:45:17 +0200

    correct function

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,22 +12,16 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node, *current;
 	unsigned int index;
 
-	if (h == NULL)
-		return (NULL);
+	if (idx == 0) /*insert at the begining*/
+		return (add_dnodeint(h, n));
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
-	if (idx == 0) /* Insert at the beginning */
-	{
-		new_node->next = *h;
-		if (*h != NULL)
-			(*h)->prev = new_node;
-		*h = new_node;
-		return (new_node);
-	}
+
 	current = *h;
 	for (index = 0; current != NULL && index < idx; index++)
 		current = current->next;
@@ -42,13 +36,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	else if (index == idx && current == NULL) /* Insert at the end */
 	{
-		current = *h;
-		while (current->next != NULL)
-			current = current->next;
-		current->next = new_node;
-		new_node->prev = current;
-		return (new_node);
+		free(new_node); /* If it's not poss.to add the new node at index idx */
+		return (add_dnodeint_end(h, n));
 	}
-	free(new_node); /* If it's not possible to add the new node at index idx */
+
+	free(new_node);
 	return (NULL);
+
 }
