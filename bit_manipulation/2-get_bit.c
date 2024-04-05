22df8d997e19a01ee8056f6b80a4089d03e729b9
Stefani-web <stephaniecvlho@gmail.com>
2024-04-05 16:59:10 +0200

    correct and lighten for Betty

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,14 +1,9 @@
 #include "main.h"
 
 /**
- * get_bit - returns the value of a bit at a given index
+ * get_bit - Returns the value of a bit at a given index
  * @n: the number to get the bit from
  * @index: the index of the bit to get, starting from 0
- *
- * Description: This function uses the bitwise AND operator to isolate the bit
- * at the given index and then shifts it right by @index positions
- * to obtain its value. If @index is greater than or equal to the
- * number of bits in @n, the function returns -1 to indicate an error
  *
  * Return: the value of the bit at index @index, or -1 if an error occurred
  */
@@ -20,5 +15,5 @@ int get_bit(unsigned long int n, unsigned int index)
 		return (-1);
 	}
 
-	return (n >> index & 1);
+	return ((n >> index) & 1);
 }
