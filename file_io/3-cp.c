85ac9fcca89485a63230306ac2ca4dbcda4689c3
Stefani-web <stephaniecvlho@gmail.com>
2024-04-06 21:04:49 +0200

    find the error to reduce the number of lines

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,5 +1,8 @@
 #include "main.h"
-#define BUFFER_SIZE 1024
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * main - Copies the content of a file to another file
@@ -12,7 +15,7 @@
 int main(int argc, char *argv[])
 {
 	int fld_from, fld_to, bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
+	char buffer[1024];
 
 	if (argc != 3)
 	{
@@ -27,34 +30,40 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	fld_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	fld_to = open(argv[2], O_WRONLY | O_CREAT |
+	O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+
 	if (fld_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(fld_from);
 		exit(99);
 	}
-
-	while ((bytes_read = read(fld_from, buffer, BUFFER_SIZE)) > 0)
+	while ((bytes_read = read(fld_from, buffer, 1024)) > 0)
 	{
 		bytes_written = write(fld_to, buffer, bytes_read);
 		if (bytes_written != bytes_read)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(fld_from);
-			close(fld_to);
 			exit(99);
 		}
 	}
-
 	if (bytes_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(fld_from);
-		close(fld_to);
 		exit(98);
 	}
+close_file_description(fld_from, fld_to);
+	return (0);
+}
 
+/**
+ * close_file_description - Funct clse fl descr & exit wth error if the case
+ * @fld_from: file description of origin
+ * @fld_to: file description ot destination
+*/
+
+void close_file_description(int fld_from, int fld_to)
+{
 	if (close(fld_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fld_from);
@@ -67,6 +76,4 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fld_to);
 		exit(100);
 	}
-
-	return (0);
 }
